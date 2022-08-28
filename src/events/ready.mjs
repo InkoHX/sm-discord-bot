50b15a00d8895e199051b94522d65d639de971d8
InkoHX <me@inkohx.dev>
2022-08-28 16:49:41 +0900

    :rewind: Revert ff1da05
    This reverts commit ff1da0503ce5bc96ba5fb729939bc30209336221.

--- src/events/ready.mjs
+++ src/events/ready.mjs
@@ -1,20 +1,14 @@
-import { on } from 'node:events'
-
-import { client as emitter } from '../index.mjs'
+import { client } from '../index.mjs'
 import {
   putGlobalCommandAll,
   putGuildCommandAll,
 } from '../interaction/index.mjs'
 
-// NOTE: https://github.com/import-js/eslint-plugin-import/issues/1759
-// eslint-disable-next-line import/newline-after-import
-;(async () => {
-  for await (const [client] of on(emitter, 'ready')) {
-    const guildId = process.env.DISCORD_GUILD_ID
+client.on('ready', client => {
+  const guildId = process.env.DISCORD_GUILD_ID
 
-    if (guildId) await putGuildCommandAll(client.application.id, guildId)
-    else await putGlobalCommandAll(client.application.id)
+  if (guildId) putGuildCommandAll(client.application.id, guildId)
+  else putGlobalCommandAll(client.application.id)
 
-    console.log(`Logged in ${client.user.tag}`)
-  }
-})().catch(console.error)
+  console.log(`Logged in ${client.user.tag}`)
+})
